d64d26d8594ac956fbcb61a087964506e7335e3f
Matt Rocco <roccoma504@gmail.com>
2017-05-31 18:55:21 -0400

    rage so hard

--- testsuite/tests/basic_tests/src/example.adb
+++ testsuite/tests/basic_tests/src/example.adb
@@ -1,8 +1,19 @@
 with AUnit.Assertions; use AUnit.Assertions;
-with Test_Example;
 
 procedure Example is
+   function Echo (PT : in Integer) return Integer;
+
+   ---------------------
+   -- List_Partitions --
+   ---------------------
+
+   function Echo (PT : in Integer) return Integer
+   is
+   begin
+      return PT;
+   end Echo;
 
 begin
-   pragma Assert (Test_Example.Function_Ex (PT => 1) = 1, "Should be 1");
+
+   pragma Assert (Echo (PT => 1) = 1, "Should be 1");
 end Example;
