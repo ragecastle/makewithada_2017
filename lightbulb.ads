a68c137776ba3ed8af36c1d53e86e11600913666
Peach <mail@jahall.me>
2017-06-13 21:33:12 -0400

    Create Simple Test
    Created a new Device, Lightbulb to test the creation of a new
    device and the state listeners.
    
    Doubles as a demo.

---
+++ lightbulb.ads
@@ -0,0 +1,11 @@
+package Lightbulb is
+
+   procedure Initialize;
+   --
+   -- Create the Lightbulb device
+
+   procedure Execute;
+   --
+   -- Do something with the Device
+
+end Lightbulb;
--- main.adb
+++ main.adb
@@ -1,19 +1,11 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with Device;
+with Lightbulb;
 
 procedure Main is
-   Lightbulb : Device.Access_Device_Type;
-   Power_State_Group : Device.Access_State_Group_Type;
-   Power_Action_Group : Device.Access_Action_Group_Type;
+
 begin
 
-   Power_State_Group := Device.Create ("Power");
-   Device.Add_State (Power_State_Group, "Off");
-   Device.Add_State (Power_State_Group, "On");
-
-   Lightbulb := Device.Create ("Lightbulb");
-   Device.Add_State_Group (Lightbulb, Power_State_Group);
-
-   Put_Line (Device.Get_Current_State (Lightbulb, "Power"));
+   Lightbulb.Initialize;
+   Lightbulb.Execute;
 
 end Main;
